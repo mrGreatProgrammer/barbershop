78aa5773718ebbf7ac0d38ea9dbd81381b1b270e
mrGreatProgrammer <fhgf9048@gmail.com>
2023-04-10 00:26:11 +0500

    slick slider started

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,14 @@
-const arrUp = document.querySelector('.arrow-up'),
-      arrDown = document.querySelector('.arrow-down')
-      subMenuEl = document.querySelector('.submenu');
+const arrUp = document.querySelector(".arrow-up"),
+  arrDown = document.querySelector(".arrow-down");
+subMenuEl = document.querySelector(".submenu");
 
-arrUp.addEventListener('click', function(event) {
-  let target = event.target;  
+arrUp.addEventListener("click", function (event) {
+  let target = event.target;
   if (target) {
-    subMenuEl.style.display = 'flex';
-    subMenuEl.style.flexDirection = 'column';
+    subMenuEl.style.display = "flex";
+    subMenuEl.style.flexDirection = "column";
   }
-})
+});
 
 // arrDown.addEventListener('click', function(event) {
 //   let target = event.target;
@@ -17,77 +17,86 @@ arrUp.addEventListener('click', function(event) {
 //   }
 // })
 
-
-
-
-
 var helpers = {
-	addZeros: function (n) {
-		return (n < 10) ? '0' + n : '' + n;
-	}
+  addZeros: function (n) {
+    return n < 10 ? "0" + n : "" + n;
+  },
 };
 
 function sliderInit() {
-  var $slider = $('.center');
-  $slider.each(function() {
+  var $slider = $(".center");
+  $slider.each(function () {
     var $sliderParent = $(this).parent();
     $(this).slick({
-      centerMode: true,
-      centerPadding: "60px",
-      slidesToShow: 3,
-      autoplay: true,
-      autoplaySpeed: 2500,
-      variableWidth: true,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 1440,
-          settings: {
-            arrows: true,
-            centerMode: true,
-            centerPadding: "40px",
-            slidesToShow: 5,
-          },
-        },
-        {
-          breakpoint: 968,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: "40px",
-            slidesToShow: 3,
-          },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: "40px",
-            slidesToShow: 1,
-            arrows: false,
-          },
-        },
-      ],
+      // dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+        // variableWidth: true,
+      // adaptiveHeight: true
     });
+    // $(this).slick({
+    //   centerMode: true,
+    //   centerPadding: "60px",
+    //   slidesToShow: 1,
+    //   autoplay: true,
+    //   autoplaySpeed: 2500,
+    //   // variableWidth: true,
+    //   slidesToScroll: 1,
+    //   responsive: [
+    //     {
+    //       breakpoint: 1440,
+    //       settings: {
+    //         arrows: true,
+    //         centerMode: true,
+    //         centerPadding: "40px",
+    //         slidesToShow: 1,
+    //       },
+    //     },
+    //     {
+    //       breakpoint: 968,
+    //       settings: {
+    //         arrows: false,
+    //         centerMode: true,
+    //         centerPadding: "40px",
+    //         slidesToShow: 1,
+    //       },
+    //     },
+    //     {
+    //       breakpoint: 480,
+    //       settings: {
+    //         arrows: false,
+    //         centerMode: true,
+    //         centerPadding: "40px",
+    //         slidesToShow: 1,
+    //         arrows: false,
+    //       },
+    //     },
+    //   ],
+    // });
 
-    if ($(this).find('.mySlides').length > 1) {
-      $(this).siblings('.slides-numbers').show();
+    if ($(this).find(".mySlides").length > 1) {
+      $(this).siblings(".slides-numbers").show();
     }
 
-    $(this).on('afterChange', function(event, slick, currentSlide){
-      $sliderParent.find('.slides-numbers .active').html(helpers.addZeros(currentSlide + 1));
+    $(this).on("afterChange", function (event, slick, currentSlide) {
+      $sliderParent
+        .find(".slides-numbers .active")
+        .html(helpers.addZeros(currentSlide + 1));
     });
 
-    var sliderItemsNum = $(this).find('.slick-slide').not('.slick-cloned').length;
-    $sliderParent.find('.slides-numbers .total').html(helpers.addZeros(sliderItemsNum));
-
+    var sliderItemsNum = $(this)
+      .find(".slick-slide")
+      .not(".slick-cloned").length;
+    $sliderParent
+      .find(".slides-numbers .total")
+      .html(helpers.addZeros(sliderItemsNum));
   });
-  
-//   $('.slick-next').on('click', function () {
-//     console.log('test');
-//     $('.slider-holder').slick('slickGoTo', 5);
-// });
-};
+
+  //   $('.slick-next').on('click', function () {
+  //     console.log('test');
+  //     $('.slider-holder').slick('slickGoTo', 5);
+  // });
+}
 
 sliderInit();

9ee2a51b7c66414764df19ad90da697dc746f624
mrGreatProgrammer <fhgf9048@gmail.com>
2023-04-09 17:33:07 +0500

    slick slider

--- scripts/app.js
+++ scripts/app.js
@@ -16,3 +16,78 @@ arrUp.addEventListener('click', function(event) {
 //     subMenuEl.style.display = 'none';
 //   }
 // })
+
+
+
+
+
+var helpers = {
+	addZeros: function (n) {
+		return (n < 10) ? '0' + n : '' + n;
+	}
+};
+
+function sliderInit() {
+  var $slider = $('.center');
+  $slider.each(function() {
+    var $sliderParent = $(this).parent();
+    $(this).slick({
+      centerMode: true,
+      centerPadding: "60px",
+      slidesToShow: 3,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      variableWidth: true,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1440,
+          settings: {
+            arrows: true,
+            centerMode: true,
+            centerPadding: "40px",
+            slidesToShow: 5,
+          },
+        },
+        {
+          breakpoint: 968,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: "40px",
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: "40px",
+            slidesToShow: 1,
+            arrows: false,
+          },
+        },
+      ],
+    });
+
+    if ($(this).find('.mySlides').length > 1) {
+      $(this).siblings('.slides-numbers').show();
+    }
+
+    $(this).on('afterChange', function(event, slick, currentSlide){
+      $sliderParent.find('.slides-numbers .active').html(helpers.addZeros(currentSlide + 1));
+    });
+
+    var sliderItemsNum = $(this).find('.slick-slide').not('.slick-cloned').length;
+    $sliderParent.find('.slides-numbers .total').html(helpers.addZeros(sliderItemsNum));
+
+  });
+  
+//   $('.slick-next').on('click', function () {
+//     console.log('test');
+//     $('.slider-holder').slick('slickGoTo', 5);
+// });
+};
+
+sliderInit();

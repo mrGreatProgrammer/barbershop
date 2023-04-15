40cdab973bba510a66c6e44eac4a9754e41d44f2
mrGreatProgrammer <fhgf9048@gmail.com>
2023-04-15 10:40:55 +0500

    createl js

--- scripts/selectorScript.js
+++ scripts/selectorScript.js
@@ -3,56 +3,114 @@ const serviseListData = [
     id: 1,
     title: "Парикмахерские услуги",
     items: [
-      { id: 1, title: "Мужской зал" },
-      { id: 2, title: "Женский зал*" },
+      {
+        id: 1,
+        title: "Мужской зал",
+        img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/751.jpg",
+      },
+      {
+        id: 2,
+        title: "Женский зал*",
+        img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/82.jpg",
+      },
     ],
-    optionItems:  [
-
+    optionItems: [
+      { id: 3, title: "" },
+      { id: 4, title: "" },
+      { id: 5, title: "" },
     ],
-    img: "hhalfg"
   },
   {
     id: 2,
     title: "Ногтевой сервис*",
     items: [
-      { id: 1, title: "Мужской зал" },
-      { id: 2, title: "Женский зал*" },
+      {
+        id: 6,
+        title: "Мужской зал",
+        img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/130.jpg",
+      },
+      {
+        id: 7,
+        title: "Женский зал*",
+        img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/114.jpg",
+      },
+    ],
+    optionItems: [
+      { id: 1, title: "" },
+      { id: 2, title: "" },
+      { id: 3, title: "" },
     ],
   },
   {
     id: 3,
     title: "Брови и ресницы*",
     items: [
-      { id: 1, title: "Мужской зал" },
-      { id: 2, title: "Женский зал*" },
+      {
+        id: 1,
+        title: "Мужской зал",
+        img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/223.jpg",
+      },
+      {
+        id: 2,
+        title: "Женский зал*",
+        img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/232.jpg",
+      },
+    ],
+    optionItems: [
+      { id: 1, title: "" },
+      { id: 2, title: "" },
+      { id: 3, title: "" },
     ],
   },
 ];
 
 const serviceLists = document.querySelector(".service__lists");
 
+function visibleSubmenu(id) {}
+
 function createServiceItem(id, title, subItems) {
   const serviceItem = document.createElement("li");
+  const serviceItemBtn = document.createElement("button");
+  serviceItemBtn.className = "service__btn";
   serviceItem.className = "service__list";
-  serviceItem.insertAdjacentHTML(
+  serviceItemBtn.insertAdjacentHTML(
     "afterbegin",
-    `
-              <div class="service__list--item">
-
-                <p class="service__list--name">${title}</p>
-                <div class="arrow-down">
-                  <svg width="19" height="11" viewBox="0 0 19 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path opacity="0.3" d="M0.5 1.00098L9.5 10.001L18.5 1.00098" stroke="black"
-                      stroke-linejoin="round" />
-                  </svg>
-                </div>
-              </div>
-              <ul class="submenu">
-                <li class="submenu__item item__active">Мужской зал</li>
-                </ul>
-                `
-    // <li class="submenu__item">Женский зал*</li>
+    `<div>
+      <div class="service__list--item">
+        <p class="service__list--name">${title}</p>
+        <div class="arrow-down">
+          <svg width="19" height="11" viewBox="0 0 19 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path opacity="0.3" d="M0.5 1.00098L9.5 10.001L18.5 1.00098" stroke="black"
+            stroke-linejoin="round" />
+          </svg>
+        </div>
+      </div>
+    </div>`
   );
+
+  function createSubmenuItem(subItem) {
+    const submenuItemEl = document.createElement("li");
+    submenuItemEl.className = "submenu__itema";
+
+    submenuItemEl.textContent = subItem.title;
+
+    return submenuItemEl;
+  }
+
+  const submenuListEl = document.createElement("ul");
+  submenuListEl.className = "submenyu";
+
+  subItems.map((e) => submenuListEl.appendChild(createSubmenuItem(e)));
+
+  serviceItem.appendChild(serviceItemBtn);
+  submenuListEl.className = "d-none";
+
+  serviceItemBtn.addEventListener("click", () => {
+    console.log(id);
+    submenuListEl.classList.toggle("d-block");
+  });
+
+  serviceItem.appendChild(submenuListEl);
   return serviceItem;
 }
 
@@ -60,16 +118,7 @@ serviseListData.map((e) =>
   serviceLists.appendChild(createServiceItem(e.id, e.title, e.items))
 );
 
-// console.log(arr)
-// serviceLists.appendChild(arr);
+const servicesImg = document.getElementsByClassName("services-img");
 
-// const a = createServiceItem();
-// serviceLists.appendChild(a);
 
-// const arrUp = document.querySelectorAll(".arrow-up"),
-//   serviceItem = document.querySelector(".service__list--item"),
-//   subMenuEl = document.querySelector(".submenu");
-
-// serviceItem.addEventListener("click", function (event) {
-
-// });
+// console.log(serviceLists);
